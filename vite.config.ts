28ca36584f4d6f24c7ead2eef0efae1f7fd25a79
leirong <643609270@qq.com>
2024-01-18 18:10:21 +0800

    chore:update publicDir

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/particle/",
+  publicDir: "/particle/",
   plugins: [react()],
   resolve: {
     alias: {
